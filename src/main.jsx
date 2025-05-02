9e25ed099c2f65cbb24c0b09d15f8b6f3b743fb4
Kishan Verma <kv57369@gmail.com>
2025-05-02 17:36:27 +0530

    rechech

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 // import { BrowserRouter } from 'react-router-dom'
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import UserContext from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <UserContext >
-  <Router>
+  <HashRouter>
     <App />
-  </Router>
+  </HashRouter>
   </UserContext>
 
 )
